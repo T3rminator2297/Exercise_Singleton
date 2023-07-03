16095788fd14f808041f5a361134601b352068c6
T3rminator2297 <francis2297@hotmail.com>
2023-07-03 10:40:58 -0400

    Classe Soleil

---
+++ src/Soleil.java
@@ -0,0 +1,15 @@
+public class Soleil
+{
+    private static Soleil instAnce;
+
+    private Soleil()
+    {
+
+    }
+
+    public static Soleil getInstance()
+    {
+        return instAnce;
+    }
+
+}
